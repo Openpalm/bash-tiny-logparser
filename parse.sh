56509b4d17919eae58e21d9101fd13f7cb7289fb
shokodemon <boo.factor@gmail.com>
2020-03-28 08:22:09 +0100

    rename reader -> parse

--- parse.sh
+++ parse.sh
@@ -7,10 +7,12 @@ outfile=hosts
 proc=$(cat $infile | cut -d$'\n' -f1 | cut -d' ' -f1 -f4 >> $tmpfile)
 
 mkdir -p ./out
+rm -rf out/*
+
 # read & write to out
 while read host addr
 do 
-  echo ${addr} >> out/$host.gifsList
+  echo ${addr} >> out/$host.txt
 done < "${tmpfile}"
 
 # cleanup
